3ffbd7c9942362e9ebede7fa4bbdd070fa906e09
ESRA-Myelin <81035058+ESRA-Myelin@users.noreply.github.com>
2021-06-24 11:03:28 +0200

    Add files via upload
    A script to compile CSV results

---
+++ compileVolBrainData.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo .................................. Compiling Data ............................................
+ls -1 | grep csv >> list ## look for all the files that have csv
+echo processing $(cat list | wc -l) files
+while read f ; do echo $f ; done < list
+cat list | head -1 >> first
+while read f ; do echo $f ; cat $f | awk NR==1 >> results.ssv ; done < first
+## while read f ; do echo $f ; cat $f | awk NR==2 >> results.ssv ; done < list
+while read f ; do echo $f ; 'cat $f | awk NR==2' >> results.ssv ; done < list
+echo ......................................... DONE ..............................................
